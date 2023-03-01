ba94d990aa93f40093c28f6ec1739b0d6816b876
tbshiki <info@tbshiki.com>
2023-03-01 10:42:47 +0900

    fix Corrections and information added

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, find_packages
-import xlwingshelper
 
-VERSION = xlwingshelper.__version__
+NAME = "xlwingshelper"
+VERSION = "0.1"
 
-setup(name="xlwingshelper", version=VERSION, packages=find_packages())
+AUTHOR = "tbshiki"
+AUTHOR_EMAIL = "info@tbshiki.com"
+URL = "https://github.com/tbshiki/" + NAME
+
+
+setup(name=NAME, author=AUTHOR, author_email=AUTHOR_EMAIL, url=URL, version=VERSION, packages=find_packages())
+
--- xlwingshelper/__init__.py
+++ xlwingshelper/__init__.py
@@ -1,4 +1,2 @@
 from .helper import *
 from .windows import *
-
-__version__ = "0.1"
